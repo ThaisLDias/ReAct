b15e90257d1d99b63524173e5a4daa553747462f
Thais Dias <dragonssaur@gmail.com>
2016-04-13 15:12:01 -0300

    v4.5

---
+++ Assets/Scripts/miniGoals.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class miniGoals : MonoBehaviour {
+
+	string[] littleGoals = new string[] {"Go to Work", "Study", "Clean my room", "Go to school", "Sleep"};
+	public Text miniG; 
+	string lg;
+
+
+	void Start () {
+		for (int i = 0; i < 4; i++) {
+			lg = littleGoals [Random.Range (0, littleGoals.Length)];
+			Debug.Log (lg);
+		}
+	}
+	
+
+	public void OnGUI() {
+		miniG = gameObject.GetComponent<Text>();
+		miniG.text = lg;
+	} 
+}
