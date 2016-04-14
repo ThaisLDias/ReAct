62d50f241db0078fb7a514141f9144e56f1e5ff3
Thais Dias <dragonssaur@gmail.com>
2016-04-13 22:25:28 -0300

    v5

---
+++ Assets/popUp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class popUp : MonoBehaviour {
+	
+	public GameObject panel; 
+	GameObject stressP;
+	private bool setActive;
+
+	void Start(){
+		//panel.SetActive (false);
+		setActive = false;
+	}
+
+	void Update(){
+		if (setActive == true) {
+			panel.SetActive (true);
+			Debug.Log ("Teste");
+		}
+	}
+		
+	void OpenPanel(){
+		stressP = GameObject.Find ("BlackHole");
+		BlackHole stressUp = stressP.GetComponent<BlackHole>(); 
+
+		if (stressUp.Stress > 20) {
+			setActive = true;
+		}	 
+	}
+
+	public void ClosePanel(){
+		panel.SetActive (false);
+		setActive = false;
+	}
+}
